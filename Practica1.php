cb8d58bc7478d5b06c8d09e2e6ed328e9b9da5ae
Andy-LH123 <116239005+Andy-LH123@users.noreply.github.com>
2024-11-06 21:31:25 -0600

    Add files via upload

---
+++ Practica1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Practica 1</title>
+</head>
+<body>
+
+<?php
+ECHO "Hola! Este es mi primer script en PHP<BR>";
+
+//Esto es una línea de comentario
+
+#Esto también es una línea de comentario
+
+/*Esto es un
+bloque de más de una linea
+de comentarios*/
+
+$vartext="Hola! Ahora estoy usando variables para guardar texto<BR>";
+echo $vartext;
+$varnum1=5;
+$varnum2=6;
+$res=$varnum1+$varnum2;
+echo "El resultado de la sum de $varnum1 y $varnum2 es = $res";
+?>
+
+</body>
+</html>

0fcb5f05e64fc31e2a60a9d6171a8aadb069df52
Andy-LH123 <116239005+Andy-LH123@users.noreply.github.com>
2024-11-18 13:00:14 -0500

    Update Practica7.php

--- Practica7.php
+++ Practica7.php
@@ -8,25 +8,7 @@
 
 	<body>
 
-		<?php
-			
-			require ("config.php");
-			echo "<h1>Practica 7: Creacion de una base de datos</h1><br><br>";
-			$conexion=mysqli_connect($servidor,$usuario,$password,$bd);
-			$basededatos="Pogramacion_Web_Andres_php_2";
-
-			$consulta="create database $basededatos";
-			if ($resultado=mysqli_query($conexion,$consulta)){
-				echo "La base de datos $basededatos, se creó con exito<br>";
-				echo "El script utilizado fue: $consulta <br><br>";
-			}
-			else{
-				"La base de datos $basededatos no se ha podido crear<br>".mysqli_error($conexion);
-			}
-			echo "<br><br>";
-			mysqli_close($conexion);
-
-		?>
+		<img src="Practica7Andres.jpg" height="800" width="1080">
 
 	</body>
 

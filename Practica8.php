022cdf76ec544e50dbeea4068b200941f389c299
Andy-LH123 <116239005+Andy-LH123@users.noreply.github.com>
2024-11-22 03:50:28 -0500

    Update Practica8.php

--- Practica8.php
+++ Practica8.php
@@ -7,26 +7,9 @@
 	</head>
 
 	<body>
-
-		<?php
-			
-			require ("config.php");
-			echo "<h1>Practica 8: Eliminación de una base de datos</h1><br><br>";
-			$conexion=mysqli_connect($servidor,$usuario,$password,$bd);
-			$basededatos="Programacion_Web_Andres_php_2";
-
-			$consulta="drop database $basededatos";
-			if ($resultado=mysqli_query($conexion,$consulta)){
-				echo "La base de datos $basededatos, se eliminó con exito<br>";
-				echo "El script utilizado fue: $consulta <br><br>";
-			}
-			else{
-				"La base de datos $basededatos no se ha podido borrar<br>".mysqli_error($conexion);
-			}
-			echo "<br><br>";
-			mysqli_close($conexion);
-
-		?>
+		<h1>Creacion de una BD</h1>
+		<br>
+		<img src="Practica8Andres.jpg" height="600" width="1080">
 
 	</body>
 
